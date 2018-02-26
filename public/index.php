3ba2e3992f11df1fff2fbbc72108d206db4b2881
Lee <lneenan@pagemasters.com.au>
2018-02-26 21:52:31 +1100

    test

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 // phpinfo();return;
+
 require_once dirname(__FILE__) . '/../vendor/autoload.php' ;
 require_once dirname(__FILE__) .'/../config/config.php';
 require_once dirname(__FILE__) .'/../config/globals.php';
